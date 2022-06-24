c0ce20cac96eab16e8a77e70ed036c5787f4a74f
Romanov Dmitriy <d.romanov@epc-it.by>
2022-06-24 12:28:35 +0300

    add hw ex with stars

---
+++ 1_Lesson/HW/ex_1_star/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex_1_star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Модуль числа {value} равен {(value < 0 ? -value : value)}");
+        }
+    }
+}
---
+++ 1_Lesson/HW/ex_2_stars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex_2_stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            decimal value = decimal.Parse(Console.ReadLine());
+            decimal reverseValue = Math.Round(1 / value,3);
+            Console.WriteLine($"Обратное значение для числа {value} равно {reverseValue}");
+        }
+    }
+}
