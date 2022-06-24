de9fcb9f4dd2f74a0aba7ec5ad46f9c365839b43
Romanov Dmitriy <d.romanov@epc-it.by>
2022-06-24 13:33:14 +0300

    hw add ex_8

---
+++ 1_Lesson/HW/ex_8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ex_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+                5 -> 2, 4
+                8 -> 2, 4, 6, 8
+            */
+            
+            int value;
+            do
+            {
+                Console.Write("Введите положительное число: ");
+                value = int.Parse(Console.ReadLine());
+                if (value <= 0)
+                {
+                    Console.WriteLine("Введено не положительное значение, повторите ввод");
+                }
+                
+            } while (value <= 0);
+            
+            int index = 2; //индекс для цикла
+
+            if (value == 1)
+            {
+                Console.WriteLine($"Для числа {value} четных чисел нет");
+                return;
+            }
+
+            Console.Write($"Для числа {value} есть след. четные числа: ");
+            while (index <= value)
+            {
+                if (index % 2 == 0)
+                {
+                    Console.Write($"{index}, ");
+                }
+                index += 2;
+            }
+        }
+    }
+}
