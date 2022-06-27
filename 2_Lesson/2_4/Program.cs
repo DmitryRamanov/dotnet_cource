ccfaf2b306e7fe0c67ae5fd270a503ab0fadf632
Romanov Dmitriy <d.romanov@epc-it.by>
2022-06-27 11:26:22 +0300

    2_4

---
+++ 2_Lesson/2_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2_4
+{
+    class Program
+    {
+        static string CheckValue(in int val)
+        {
+            if (val % 7 == 0 && val % 23 == 0)
+            {
+                return "ДА";
+            }
+            
+            return "НЕТ";
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число ");
+            int val = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Результат проверки: {CheckValue(in val)}");
+        }
+    }
+}
