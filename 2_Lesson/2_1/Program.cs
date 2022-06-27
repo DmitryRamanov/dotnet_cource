9df762d6c65de75abf5364ee9b4c379724cf33f0
Romanov Dmitriy <d.romanov@epc-it.by>
2022-06-27 10:52:50 +0300

    2_1

---
+++ 2_Lesson/2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_1
+{
+    class Program
+    {
+        static int GetLastSimbol(int value)
+        {
+            return (value % 10);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число ");
+            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Последний символ числа {value} равен {GetLastSimbol(value)}");
+        }
+    }
+}
