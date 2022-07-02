36d3027bcb72ff848bf4ebd65f8923a6477f0309
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-02 14:38:34 +0300

    3_Lesson 3_4

---
+++ 3_Lesson/3_4/Program.cs
@@ -0,0 +1,11 @@
+﻿void numberSquares(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i,2)}, ");
+    }
+}
+
+numberSquares(5); //1,4,9,16,25
+Console.WriteLine();
+numberSquares(2); //1,4
