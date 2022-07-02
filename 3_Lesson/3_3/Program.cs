804221fd062f8b4e4735ad38f5cf867d109b0898
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-02 14:32:14 +0300

    3_Lesson 3_3

---
+++ 3_Lesson/3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    2. Напишите программу, которая принимает на вход координаты
+    двух точек и находит расстояние между ними в 2D пространстве.
+*/
+
+void getDistance2D(double aX, double aY, double bX, double bY)
+{
+    double result = Math.Sqrt(Math.Pow(bX - aX,2) + Math.Pow(bY - aY,2));
+    Console.WriteLine($"Расстояние между точками A({aX},{aY}) и B({bX},{bY}) равно {Math.Round(result,2)}");
+}
+
+getDistance2D(3, 6, 2, 1); //5.09
+getDistance2D(7, -5, 1, 1); //7.21
