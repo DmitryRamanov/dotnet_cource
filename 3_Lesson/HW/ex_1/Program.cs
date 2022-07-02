556d55b6caf1bf9dc8bf3567d8dcd59e517ec2d3
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-02 16:02:34 +0300

    HW ex_1

---
+++ 3_Lesson/HW/ex_1/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+bool checkingIfNumberIsPalindrome(long number)
+{
+    bool result = false;
+    if (number > 0)
+    {
+        string numberStr = number.ToString();
+        int strLength = numberStr.Length;
+        for (int i = 0; i < strLength / 2; i++)
+        {
+            if (numberStr[i] != numberStr[strLength - i - 1])
+            {
+                result = false;
+                break;
+            }
+            else
+            {
+                result = true;
+            }
+        }
+    }
+
+    return result;
+}
+
+long number = 14212;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
+number = 23432;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
+number = 12821;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
+number = 983389;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
+number = 2222222222;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
+number = 19311113231111391;
+Console.WriteLine($"{number} -> {checkingIfNumberIsPalindrome(number)}");
