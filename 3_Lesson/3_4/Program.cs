89d34504ca3b8b2a7b788ca2b47c5867c72b7818
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-02 14:39:41 +0300

    add desription 3_Lesson 3_4

--- 3_Lesson/3_4/Program.cs
+++ 3_Lesson/3_4/Program.cs
@@ -1,4 +1,9 @@
-﻿void numberSquares(int n)
+﻿/*
+    Напишите программу, которая принимает на вход число (N)
+    и выдаёт таблицу квадратов чисел от 1 до N.
+*/
+
+void numberSquares(int n)
 {
     for (int i = 1; i <= n; i++)
     {
