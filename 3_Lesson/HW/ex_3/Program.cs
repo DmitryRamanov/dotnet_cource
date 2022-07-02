2910e1ac1cfa4dbe3b0aeea840041ea19f986482
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-02 16:14:43 +0300

    HW ex_3

---
+++ 3_Lesson/HW/ex_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void numberSquares(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
+numberSquares(3); //1, 8, 27
+Console.WriteLine();
+numberSquares(10); //1, 8, 27, 64, 125
