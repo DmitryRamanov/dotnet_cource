19ee8f0ae876a044a33644053f4931cf5b79d4bb
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-04 11:01:36 +0300

    hw ex_1 and ex_3

---
+++ 4_Lesson/HW/ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+double GetPow(double baseValue, double extentValue)
+{
+    double result = baseValue;
+
+    for (int i = 2; i <= extentValue; i++)
+    {
+        result *= baseValue;
+    }
+
+    return result;
+}
+
+Console.WriteLine(GetPow(3,5));
+Console.WriteLine(GetPow(2,4));
