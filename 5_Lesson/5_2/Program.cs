f3bfb94b2db2429fed426b47d7fe3abac9ff0f8e
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-07 10:33:22 +0300

    5_2

---
+++ 5_Lesson/5_2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Напишите программу замена элементов массива:
+положительные элементы замените на соответствующие
+отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] CreateNewArray(int arrayLength)
+{
+    return new int[arrayLength];
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 9);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void ChangePositiveNumbersOnNegativeInArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+int[] array = CreateNewArray(12);
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangePositiveNumbersOnNegativeInArray(array);
+PrintArray(array);
