99992ef787ca58d1660fbcdb763da4530b530e59
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-11 10:47:37 +0300

    6_2

---
+++ 6_Lesson/6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+bool CheckTriangle(int valueA, int valueB, int valueC)
+{
+    if (valueA > valueB + valueC)
+    {
+        return false;
+    }
+    if (valueB > valueA + valueC)
+    {
+        return false;
+    }
+    if (valueC > valueB + valueA)
+    {
+        return false;
+    }
+    return true;
+}
+
+int valueA = 9;
+int valueB = 3;
+int valueC = 5;
+
+Console.WriteLine($"Может-ли существовать треугольник со сторонами ({valueA},{valueB},{valueC}) -> {CheckTriangle(valueA, valueB, valueC)}");
