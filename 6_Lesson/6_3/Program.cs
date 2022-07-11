371c016e7fe6664a5bb43120b877f279df4ae23a
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-11 11:07:47 +0300

    6_3

---
+++ 6_Lesson/6_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string Conert10Into2(int value)
+{
+    string result = String.Empty;
+    do
+    {
+        result = (value % 2).ToString() + result;
+        value = value / 2;
+    } while (value > 0);
+
+    return result;
+}
+
+int valueIn10 = 10;
+Console.WriteLine($"Числов {valueIn10} в двоичной систем -> {Conert10Into2(valueIn10)}");
