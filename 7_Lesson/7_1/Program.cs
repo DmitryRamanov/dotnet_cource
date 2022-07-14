3a1130b28905eb88cbb4095452ab05e53cf78356
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-14 10:25:05 +0300

    add 7_Lesson empty and 7_1

---
+++ 7_Lesson/7_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] CreateEmpty2DArray(int rowsNumber, int columnsNumber)
+{
+    return new int[rowsNumber, columnsNumber];
+}
+
+void Fill2DArray(int[,] array2D, int minBorderForRandom, int maxBorderForRandom)
+{
+    int rowsNumber = array2D.GetLength(0);
+    int columnsNumber = array2D.GetLength(1);
+
+    for (int row_index = 0; row_index < rowsNumber; row_index++)
+    {
+        for (int column_index = 0; column_index < columnsNumber; column_index++)
+        {
+            array2D[row_index, column_index] = new Random().Next(minBorderForRandom, maxBorderForRandom);
+        }
+    }
+}
+
+void Print2DArray(int[,] array2D)
+{
+    int rowsNumber = array2D.GetLength(0);
+    int columnsNumber = array2D.GetLength(1);
+
+    for (int row_index = 0; row_index < rowsNumber; row_index++)
+    {
+        for (int column_index = 0; column_index < columnsNumber; column_index++)
+        {
+            Console.Write($"{array2D[row_index, column_index],4:D} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2D = CreateEmpty2DArray(3, 4);
+Fill2DArray(array2D, -100, 100);
+Print2DArray(array2D);
