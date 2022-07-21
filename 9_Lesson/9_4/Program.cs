dddd02dbaf7199b77ee2e6264c7d74fbcab30778
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-21 11:07:20 +0300

    9_4

---
+++ 9_Lesson/9_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5-> 243(3⁵)
+// A = 2; B = 3-> 8
+
+int GetTheExponentOfANumber(int valueA, int valueB)
+{
+    if (valueB == 0)
+    {
+        return 1;
+    }
+    return GetTheExponentOfANumber(valueA, valueB - 1) * valueA;
+}
+
+Console.Write("Введите значение A: ");
+int valueA = int.Parse(Console.ReadLine());
+Console.Write("Введите значение B: ");
+int valueB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A = {valueA}, B = {valueB} -> {GetTheExponentOfANumber(valueA, valueB)}");
