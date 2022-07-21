be5f0b9d0be8a2ca2cd626d28f1640d986c1bf16
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-21 14:29:25 +0300

    hw ex_2

---
+++ 9_Lesson/HW/ex_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15-> 120
+// M = 4; N = 8-> 30
+
+int GetAmountNaturalNumbers(int valueStart, int valueEnd)
+{
+    if (valueStart > valueEnd)
+    {
+        return 0;
+    }
+    if (valueStart == valueEnd)
+    {
+        return valueEnd;
+    }
+    return valueStart + GetAmountNaturalNumbers(valueStart + 1, valueEnd);
+}
+
+Console.Write("Введите значение M: ");
+int valueM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int valueN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {valueM}, N = {valueN} -> {GetAmountNaturalNumbers(valueM, valueN)}");
+if (valueM > valueN)
+{
+    Console.Write("Число M, начало промежутка, больше числа N, окончания промежутка.");
+}
