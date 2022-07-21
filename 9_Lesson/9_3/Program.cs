a6fe79d199d1ab77298e02c8fc539558f75ab156
Romanov Dmitriy <d.romanov@epc-it.by>
2022-07-21 11:00:29 +0300

    9_3

---
+++ 9_Lesson/9_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int GetSumNumbers(int value)
+{
+    if (value == 0)
+    {
+        return 0;
+    }
+    return GetSumNumbers(value / 10) + value % 10;
+}
+
+Console.Write("Введите значение N: ");
+int valueN = int.Parse(Console.ReadLine());
+Console.WriteLine($"N = {valueN} -> {GetSumNumbers(valueN)}");
